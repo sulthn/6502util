4e2566735d45525b8f472418e19f3ef9ed6caf51
sulthn <ASUS@WIRELESSHOSE>
2023-09-02 19:54:32 +0700

    commit

--- test/test.s
+++ test/test.s
@@ -1,11 +1,20 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
   .org $8000
 
 reset:
   ldx #$ff
   txs
+
+  lda #$ff
+  sta DDRA
+  sta DDRB
   
-  lda #12
-  sta $00
+  lda #$aa
+  sta PORTA
 loop:
   jmp loop
 
